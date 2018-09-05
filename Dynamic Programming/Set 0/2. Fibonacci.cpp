0883826413ec37cc992969984d8b3f0111f898c8
Himank Jog <himank.juit@gmail.com>
2018-09-06 01:07:46 +0530

    Updated array name in 2. Fibonacci

--- Dynamic Programming/Set 0/2. Fibonacci.cpp
+++ Dynamic Programming/Set 0/2. Fibonacci.cpp
@@ -2,18 +2,18 @@
 #define li long long int
 using namespace std;
 
-li fibo[10000] = {0};
+li memo[10000] = {0};
 
 li fib(int n){
-	if(fibo[n]) return fibo[n];
-	return fibo[n] = fib(n-1) + fib(n-2);
+	if(memo[n]) return memo[n];
+	return memo[n] = fib(n-1) + fib(n-2);
 }
 
 int main(int argc, char const *argv[])
 {
 	int n;
 	cin >> n;
-	fibo[0] = fibo[1] = 1;
+	memo[0] = memo[1] = 1;
 	cout << fib(n) << endl;
 	return 0;
 }
